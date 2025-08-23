a3af4696593979ce804857e4a229e059b81e96c3
NotAI <personal@notai.xyz>
2025-08-23 12:49:46 -0400

    updated treesitter related plugins to modern versions

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,22 +1,34 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
     lazy = false,
     build = ':TSUpdate',
+    branch = 'main',
+    ---@module 'nvim-treesitter'
+    ---@type TSConfig
     opts = {
+      install_dir = vim.fs.joinpath(vim.fn.stdpath 'data', 'site'),
       auto_install = true,
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
       },
     },
-    config = function(_, opts)
-      require('nvim-treesitter.configs').setup(opts)
-    end,
   },
-  'nvim-treesitter/playground',
-  'nvim-treesitter/nvim-treesitter-context',
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    ---@module 'treesitter-context'
+    ---@type TSContext.UserConfig
+    opts = {},
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+    },
+    branch = 'main',
+    ---@module 'nvim-treesitter-textobjects'
+    ---@type TSTextObjects.UserConfig
+    opts = {},
+  },
 }
