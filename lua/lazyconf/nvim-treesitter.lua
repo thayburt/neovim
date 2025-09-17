795e0ce7ce3b2564281c81362154b12f614473b6
NotAI <notai@notai.xyz>
2025-09-17 10:03:25 -0400

    Updated treesitter dependant config

--- lua/lazyconf/nvim-treesitter.lua
+++ lua/lazyconf/nvim-treesitter.lua
@@ -24,3 +24,11 @@ require('nvim-treesitter').install {
   'yaml',
   'zig',
 }
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { '<filetype>' },
+  callback = function()
+    vim.treesitter.start()
+  end,
+})
+vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
